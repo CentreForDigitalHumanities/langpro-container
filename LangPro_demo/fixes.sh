8bb6f9ad6d53f741f35907d4e62d4159ab14f876
ben <b.bonfil@uu.nl>
2025-07-23 15:35:36 +0200

    remove prolog gui import when running in container

--- LangPro_demo/fixes.sh
+++ LangPro_demo/fixes.sh
@@ -14,6 +14,12 @@ if [ ! -d chunk ]; then
 fi
 popd
 
+# disable prolog GUI model
+pushd /git_LangPro
+sed -i "\|use_module(library(pce))|d" prolog/printer/gui_tree.pl
+sed -i "\|displayTree/3|d" prolog/prover/tt_nattableau.pl
+popd
+
 # create langpro_bin binary on-fly because the binary
 # should be compatible with the local swipl version; no +x is needed.
 echo "Compiling langpro binary...";
