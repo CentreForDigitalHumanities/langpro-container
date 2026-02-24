67ccd3a2109b687f99e102fd8f006f690cd19374
Julian Gonggrijp <j.gonggrijp@uu.nl>
2026-02-24 03:30:59 +0100

    Revert "DEBUG try top-level models"
    This reverts commit e6f5d3d5467b552a1f7e9bc25fa14d47d471bf0b.

--- LangPro_demo/langpro_demo.py
+++ LangPro_demo/langpro_demo.py
@@ -380,7 +380,7 @@ def cc_parsing(sen, v=0):
     '''
     sen_esc = sen.replace('"', '\\"')
     path = 'parsers/candc'
-    cmd = ('{0}/candc/bin/candc  --models {0}/models '
+    cmd = ('{0}/candc/bin/candc  --models {0}/models/models '
            '--candc-printer boxer  --candc-parser-noisy_rules=false'
            ).format(path)
     import os
